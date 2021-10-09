2a03288538705608e7befd2abeca79a28508d186
Ashwin Rajput <rajputashwin011@gmail.com>
2021-10-10 02:49:49 +0530

    add heading text and logo

--- frontend-react/src/components/LandingPage.jsx
+++ frontend-react/src/components/LandingPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/landingPage.css'
-
+import ACMlogo from '../images/acm-logo.webp'
 export default function LandingPage() {
+    const [ arr, useArr ] = useState()
+
     return (
-        <div id="hero">
-            {/* <img src={bgImg} alt=""/> */}
+        <div>
+            <div id="hero"></div>
+            <div className="content">
+                <div className="part1">
+                    <h1>ACM Student Chapter</h1>
+                    <h2>#include {"<"}{arr}{">"}</h2>
+                </div>
+                <div className="part2">
+                    <img src={ ACMlogo } alt="ACM logo"/>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
