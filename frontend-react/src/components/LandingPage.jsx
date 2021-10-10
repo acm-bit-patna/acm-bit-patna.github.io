fbedfc24dc2f3df86992b9233f27c84506b76f7f
Ashwin Rajput <rajputashwin011@gmail.com>
2021-10-11 02:14:06 +0530

    small changes in css and add comments

--- frontend-react/src/components/LandingPage.jsx
+++ frontend-react/src/components/LandingPage.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useRef } from 'react'
 import '../css/landingPage.css'
 import ACMlogo from '../images/acm-logo.webp'
-import { motion } from 'framer-motion'
 import Typed from 'typed.js'
 
 export default function LandingPage() {
+    //Adding tags in this array will display them on the landing page
+    const tags = ['Algorithms', 'Maths', 'Adhoc', 'Greedy', 'Dp', 'Graphs']
+
+
     const el = useRef()
 
     useEffect(() => {
         const typed = new Typed(el.current, {
-            strings: ['Algorithms', 'Maths', 'Adhoc', 'Greedy', 'Dp', 'Graphs'],
+            strings: tags,
             startDelay: 300,
             typeSpeed: 100,
             backSpeed: 50,
             backDelay: 2000,
             loop: true,
-            smartBackspace: true
+            smartBackspace: true,
+            autoInsertCss: true
         })
         return () => {
             typed.destroy()
@@ -24,7 +28,7 @@ export default function LandingPage() {
     
     return (
         <div>
-            <div id="hero"></div>
+            <div className="landing-bg"></div>
             <div className="content">
                 <div className="content-part1">
                     <h1>ACM Student Chapter</h1>
