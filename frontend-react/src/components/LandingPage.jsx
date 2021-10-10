fc2b4b600a5070aaa0eaab43c1b236d49f3db109
Ashwin Rajput <rajputashwin011@gmail.com>
2021-10-11 01:49:29 +0530

    add typing animation and change font

--- frontend-react/src/components/LandingPage.jsx
+++ frontend-react/src/components/LandingPage.jsx
@@ -1,18 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import '../css/landingPage.css'
 import ACMlogo from '../images/acm-logo.webp'
-export default function LandingPage() {
-    const [ arr, useArr ] = useState()
+import { motion } from 'framer-motion'
+import Typed from 'typed.js'
 
+export default function LandingPage() {
+    const el = useRef()
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['Algorithms', 'Maths', 'Adhoc', 'Greedy', 'Dp', 'Graphs'],
+            startDelay: 300,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000,
+            loop: true,
+            smartBackspace: true
+        })
+        return () => {
+            typed.destroy()
+        }
+    }, [])
+    
     return (
         <div>
             <div id="hero"></div>
             <div className="content">
-                <div className="part1">
+                <div className="content-part1">
                     <h1>ACM Student Chapter</h1>
-                    <h2>#include {"<"}{arr}{">"}</h2>
+                    <p>#include {"<"}
+                        <span className="typed" ref={el}/>
+                    {">"}</p>
                 </div>
-                <div className="part2">
+                <div className="content-part2">
                     <img src={ ACMlogo } alt="ACM logo"/>
                 </div>
             </div>
